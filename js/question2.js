f930d111de1087fc63a91c4304e72a26b722203d
IamMattDamon <frochi@gmail.com>
2020-02-16 19:13:46 +0100

    first commit

---
+++ js/question2.js
@@ -0,0 +1,33 @@
+// JS1 - Module 4 - MA4 - Question 2
+
+const callUrl = "https://api.rawg.io/api/creators";
+
+fetch(callUrl)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(json) {
+        createGames(json);
+    })
+    .catch(function(error) {
+        console.log(error);
+    });
+
+    function createGames(json) {
+        const results = json.results;
+        console.dir(results);
+
+        const resultsContainer = document.querySelector(".results");
+
+        let html = "";
+
+        results.forEach(function(result) {
+            html += `<div class="game">
+                        <h2>${result.name}</h2>
+                        <img src="${result.background_image}" alt="${result.name}">
+                    </div>`;
+        });
+
+        resultsContainer.innerHTML = html;
+    }
+
